bfc5aae320de07088a190119169361fb09152ebd
FlatIron <flatiron@Lisas-MacBook-Air.local>
2018-08-31 18:26:58 +0100

    Done.

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,11 @@
 class CashRegister
-  def initialize(total = 0, discount = 0)
+  attr_accessor :total, :discount
+
+  def initialize(total = 0, discount = 20)
     @total = total
-    @discount = @total * discount / 100
+    @discount = discount
+  end
+
+  def add_item
   end
 end
